65b42d3b02093ea7026d5b472a9ed37c781c3a69
Simon Duvergier <simon.duvergier@mobile-devices.fr>
2017-02-09 11:58:50 +0100

    AuthHash: use uid of the user instead of id.

--- lib/omniauth/strategies/munic.rb
+++ lib/omniauth/strategies/munic.rb
@@ -15,7 +15,7 @@ module OmniAuth
             # additional calls (if the user id is returned with the token
             # or as a URI parameter). This may not be possible with all
             # providers.
-            uid { raw_info['id'] }
+            uid { raw_info['uid'] }
 
             info do
                 i = {
